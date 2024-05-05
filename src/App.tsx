e7f9d45a6d529babcbba924f7fa7551d4e9e6a40
badalkr2004 <badalkr279@gmail.com>
2024-05-06 00:31:03 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1>hello vite</h1>
+      <Button variant={"destructive"}>hi, Badal</Button>
     </>
   );
 }
