7cd47aa1d5203c77a860f536eb745c07d0ad6c51
badalkr2004 <badalkr279@gmail.com>
2024-05-06 02:51:42 +0530

    display error while login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -47,8 +47,12 @@ const LoginPage = () => {
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account.
-            {mutation.isPending && <p>loading...</p>}
+            Enter your email below to login to your account. <br />
+            {mutation.isError && (
+              <span className="text-red-500 text-sm">
+                Something went wrong.
+              </span>
+            )}
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
